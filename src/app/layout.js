45b9c06686e91f77e06a8cc2dc09576beec24fc7
Herminio Henriques <herminiohenriques@gmail.com>
2023-09-05 04:05:49 +0000

    Update 41 files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,3 @@
-import Navbar from "@/components/navbar/Navbar";
-import "./globals.css";
-import { Inter, Roboto, Poppins } from "next/font/google";
-import Footer from "@/components/footer/Footer";
-import { ThemeProvider } from "@/context/ThemeContext";
-import AuthProvider from "@/components/AuthProvider/AuthProvider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Lama Dev",
@@ -15,16 +7,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider>
-          <AuthProvider>
+      <body>
             <div className="container">
-              <Navbar />
               {children}
-              <Footer />
             </div>
-          </AuthProvider>
-        </ThemeProvider>
       </body>
     </html>
   );
